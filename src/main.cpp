ee9c9aa9780970c68f52dece8cff09de41edbffc
GiantStepz <acoop805@gmail.com>
2025-04-13 23:04:45 -0700

    Now search for macro definition in line, then pass full block to  parser

--- src/main.cpp
+++ src/main.cpp
@@ -23,9 +23,30 @@ int main(int argc, char *argv[])
     HossSpice engine;
     Parser parser(engine);
     std::string line;
+    std::string block;
+    bool in_macro = false;
     while (std::getline(file, line))
     {
-        parser.parseLine(line);
+        if (in_macro)
+        {
+            block += line + "\n";
+            if (line.find('}') != std::string::npos)
+            {
+                // full macro block captured
+                parser.parseLine(block);
+                block.clear();
+                in_macro = false;
+            }
+        } // if macro def is found, the full block is needed for the line
+        else if (line.find("macro") == 0)
+        {
+            block = line + "\n";
+            in_macro = true;
+        }
+        else
+        {
+            parser.parseLine(line);
+        }
     }
     engine.run();
 }
